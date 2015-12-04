01a0a75577fda162764a6e8fd8743fb3c3bc1504
Thomas Didierjean <thomas.didierjean@gmail.com>
2015-12-04 14:08:12 +0000

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,12 +1,14 @@
-var d = new Date();
-var n = d.getHours();
+window.onload = function(){
+  var d = new Date();
+  var n = d.getHours();
 
-if (n == 16) {
-  document.getElementById("cookie-status").innerHTML = 'YES!!';
-} else if (n == 15) {
-  document.getElementById("cookie-status").innerHTML = 'Almost...';
-} else if (n == 17) {
-  document.getElementById("cookie-status").innerHTML = 'Too late :(';
-} else {
-  document.getElementById("cookie-status").innerHTML = 'No';
+  if (n == 16) {
+    document.getElementById("cookie-status").innerHTML = 'YES!!';
+  } else if (n == 15) {
+    document.getElementById("cookie-status").innerHTML = 'Almost...';
+  } else if (n == 17) {
+    document.getElementById("cookie-status").innerHTML = 'Too late :(';
+  } else {
+    document.getElementById("cookie-status").innerHTML = 'No';
+  }
 }
